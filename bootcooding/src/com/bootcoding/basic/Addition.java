77f45e96dceab41774933ec830f67d28ffe2a3b5
sujitchichate <sujitchichate@gmail.com>
2022-08-18 09:14:45 +0530

    addition sucessful

---
+++ bootcooding/src/com/bootcoding/basic/Addition.java
@@ -0,0 +1,10 @@
+package com.bootcoding.basic;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 5;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
