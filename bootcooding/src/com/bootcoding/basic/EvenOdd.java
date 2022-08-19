e9548ab110cb7fcdae63ca974cad2bd5fc7648fa
sujitchichate <sujitchichate@gmail.com>
2022-08-19 08:00:06 +0530

    program sucessful

---
+++ bootcooding/src/com/bootcoding/basic/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.bootcoding.basic;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n=11;
+        if (n%2==0){
+            System.out.println("number is even");
+
+        }
+        else {
+            System.out.println("number is odd");
+        }
+    }
+}
