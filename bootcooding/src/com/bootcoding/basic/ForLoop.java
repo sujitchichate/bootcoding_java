b41cfac4e8d3c363ededd9fae950ca0d91bb6879
sujitchichate <sujitchichate@gmail.com>
2022-08-19 09:25:45 +0530

    program sucessful

---
+++ bootcooding/src/com/bootcoding/basic/ForLoop.java
@@ -0,0 +1,10 @@
+package com.bootcoding.basic;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
