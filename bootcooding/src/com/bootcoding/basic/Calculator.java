cfd03491c4f3500df5798fa64b428d3541205450
sujitchichate <sujitchichate@gmail.com>
2022-08-19 09:24:08 +0530

    program sucessful

---
+++ bootcooding/src/com/bootcoding/basic/Calculator.java
@@ -0,0 +1,33 @@
+package com.bootcoding.basic;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Add :"+Calculator(10, 20, "Add"));
+        System.out.println("Mul :"+Calculator(10, 20, "Mul"));
+        System.out.println("Sub :"+Calculator(10, 20, "Sub"));
+        System.out.println("Div :"+Calculator(10, 20, "Div"));
+//        int add = Calculator(10, 20, "Add");
+//        int mul = Calculator(10, 20, "Mul");
+//        int sub = Calculator(10, 20, "Sub");
+//        int div = Calculator(10, 20, "Divi");
+//
+//        System.out.println(add);
+//        System.out.println(Mul);
+//        System.out.println(Sub);
+//        System.out.println(Divi);
+    }
+
+   static int Calculator(int n, int m, String action) {
+        if (action == "Add") {
+            return n + m;
+        } else if (action == "Mul") {
+            return n * m;
+        } else if (action == "Sub") {
+            return n - m;
+        } else if (action == "Div") {
+            return n / m;
+        }else
+        return n;
+    }
+
+}
