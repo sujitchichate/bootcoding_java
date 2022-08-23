d4b6169678c0509e360a14f883315a101762e1b2
sujitchichate <sujitchichate@gmail.com>
2022-08-23 23:38:34 +0530

    array printed in reverse order

---
+++ bootcooding/src/com/bootcoding/basic/Array5.java
@@ -0,0 +1,12 @@
+package com.bootcoding.basic;
+
+public class Array5 {
+    public static void main(String[] args) {
+        int a[]={10,15,20,25,35,75,80,95};
+        for(int i=7;i<a.length;--i){
+            System.out.println(a[i]);
+
+        }
+
+    }
+}
