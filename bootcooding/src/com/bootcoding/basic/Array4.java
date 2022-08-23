c3825f107f8a9d25963778869a14f300365d4c4c
sujitchichate <sujitchichate@gmail.com>
2022-08-23 09:37:32 +0530

    - Created program for array

---
+++ bootcooding/src/com/bootcoding/basic/Array4.java
@@ -0,0 +1,15 @@
+package com.bootcoding.basic;
+
+public class Array4 {
+    public static void main(String[] args) {
+        int a[]={10,15,20,25,35,75,80,95};
+        //int n;
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0){
+                System.out.println(a[i]);
+
+            }
+
+        }
+    }
+}
