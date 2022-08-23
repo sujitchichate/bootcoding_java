ac2245569f8868ac0821aebb41b9a32cd4c2a6f7
sujitchichate <sujitchichate@gmail.com>
2022-08-23 09:21:19 +0530

    - Created program for array

--- bootcooding/src/com/bootcoding/basic/Array1.java
+++ bootcooding/src/com/bootcoding/basic/Array1.java
@@ -4,7 +4,7 @@ public class Array1 {
     public static void main(String[] args) {
         int a[]={10,15,20,25,30};
         for(int i=0;i<a.length;i++){
-            System.out.println(a[i]);
+            System.out.print(a[i]+" ");
 
         }
     }
