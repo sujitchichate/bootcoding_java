82a8b93f6fcabcc8cf69f3aba6780f603051f24e
sujitchichate <sujitchichate@gmail.com>
2022-08-23 09:39:45 +0530

    printed array elements in single line

--- bootcooding/src/com/bootcoding/basic/Array4.java
+++ bootcooding/src/com/bootcoding/basic/Array4.java
@@ -6,7 +6,7 @@ public class Array4 {
         //int n;
         for(int i=0;i<a.length;i++){
             if(a[i]%2==0){
-                System.out.println(a[i]);
+                System.out.print(a[i]+" ");
 
             }
 
