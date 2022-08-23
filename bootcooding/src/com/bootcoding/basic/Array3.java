130092a5c753ff27a99a3b3f4b3c61c2b6f68621
sujitchichate <sujitchichate@gmail.com>
2022-08-23 09:10:05 +0530

    - Created program for array

---
+++ bootcooding/src/com/bootcoding/basic/Array3.java
@@ -0,0 +1,10 @@
+package com.bootcoding.basic;
+
+public class Array3 {
+    public static void main(String[] args) {
+        String a[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
