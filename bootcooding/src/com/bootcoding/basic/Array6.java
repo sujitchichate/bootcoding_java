88d04dfa92b1f735539287b5299953081e57c0fc
sujitchichate <sujitchichate@gmail.com>
2022-08-24 09:08:20 +0530

    finding particular element in array by simple method

--- bootcooding/src/com/bootcoding/basic/Array6.java
+++ bootcooding/src/com/bootcoding/basic/Array6.java
@@ -2,10 +2,19 @@ package com.bootcoding.basic;
 
 public class Array6 {
     public static void main(String[] args) {
-int a[]={10,15,20,25,35,75,80,95};
-for(i=0;i<a.length;i++){
+        int a[] = {10, 15, 20, 25, 35, 75, 180, 95};
+        boolean found=false;
+        for (int i = 0; i < a.length; i++) {
+            if (80 == a[i]) {
+                found=true;//overriding
 
-}
+            }
+        }
+        if (found) {
+            System.out.println("found");
+        }else{
+            System.out.println("not found");
+        }
+
     }
-
 }
