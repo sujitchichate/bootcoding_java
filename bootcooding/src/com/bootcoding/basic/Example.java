9597720ec92285bfa5e47a72d41fa975910696e3
sujitchichate <sujitchichate@gmail.com>
2022-08-24 20:39:08 +0530

    finding particular element in array by practical method

---
+++ bootcooding/src/com/bootcoding/basic/Example.java
@@ -0,0 +1,9 @@
+package com.bootcoding.basic;
+
+public class Example {
+    public static void main(String[] args) {
+        String s1="Raja";
+        int l=s1.length();
+        System.out.println(l);
+    }
+}
