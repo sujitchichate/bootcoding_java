1b55d5c309865497ab8932288ff721d9b9c43f49
sujitchichate <sujitchichate@gmail.com>
2022-08-27 20:39:12 +0530

    Swipe first and last element in array test cases is successful

---
+++ bootcooding/src/com/bootcoding/basic/Sample1.java
@@ -0,0 +1,9 @@
+package com.bootcoding.basic;
+
+public class Sample1 {
+    public String[] separateWord(String str){
+        //String str[]={"My,name"};
+        String str1[]=str.split(",");
+        return (str1);
+    }
+}
